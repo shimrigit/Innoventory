ada474318c6f0e39f35b6abf3f9f066763c2fbcb
shimrigit <shimri.lotan@gmail.com>
2026-06-23 13:32:01 +0300

    Bug fix

--- scripts/Check invoices in subdirectories.ps1
+++ scripts/Check invoices in subdirectories.ps1
@@ -18,13 +18,27 @@ $dec = [char]0x05D3 + [char]0x05E6 + [char]0x05DE + [char]0x05D1 + [char]0x05E8
 $jan = [char]0x05D9 + [char]0x05E0 + [char]0x05D5 + [char]0x05D0 + [char]0x05E8
 $feb = [char]0x05E4 + [char]0x05D1 + [char]0x05E8 + [char]0x05D5 + [char]0x05D0 + [char]0x05E8
 $mar = [char]0x05DE + [char]0x05E8 + [char]0x05E5
+$apr = [char]0x05D0 + [char]0x05E4 + [char]0x05E8 + [char]0x05D9 + [char]0x05DC
+$may = [char]0x05DE + [char]0x05D0 + [char]0x05D9
+$jun = [char]0x05D9 + [char]0x05D5 + [char]0x05E0 + [char]0x05D9
+$jul = [char]0x05D9 + [char]0x05D5 + [char]0x05DC + [char]0x05D9
+$aug = [char]0x05D0 + [char]0x05D5 + [char]0x05D2 + [char]0x05D5 + [char]0x05E1 + [char]0x05D8
+$sep = [char]0x05E1 + [char]0x05E4 + [char]0x05D8 + [char]0x05DE + [char]0x05D1 + [char]0x05E8
+$oct = [char]0x05D0 + [char]0x05D5 + [char]0x05E7 + [char]0x05D8 + [char]0x05D5 + [char]0x05D1 + [char]0x05E8
 
-$novemberTotal = 0
-$decemberTotal = 0
-$januaryTotal  = 0
-$februaryTotal = 0
-$marchTotal    = 0
-$allPdfTotal   = 0
+$novemberTotal  = 0
+$decemberTotal  = 0
+$januaryTotal   = 0
+$februaryTotal  = 0
+$marchTotal     = 0
+$aprilTotal     = 0
+$mayTotal       = 0
+$juneTotal      = 0
+$julyTotal      = 0
+$augustTotal    = 0
+$septemberTotal = 0
+$octoberTotal   = 0
+$allPdfTotal    = 0
 
 $subDirs = Get-ChildItem -Path $rootFolder -Directory | Sort-Object Name
 
@@ -46,11 +60,18 @@ foreach ($dir in $subDirs) {
 
     foreach ($file in $pdfFiles) {
         $fileName = $file.Name
-        if ($fileName -like ('*' + $nov + '*')) { $novemberTotal++ }
-        if ($fileName -like ('*' + $dec + '*')) { $decemberTotal++ }
-        if ($fileName -like ('*' + $jan + '*')) { $januaryTotal++  }
-        if ($fileName -like ('*' + $feb + '*')) { $februaryTotal++ }
-        if ($fileName -like ('*' + $mar + '*')) { $marchTotal++    }
+        if ($fileName -like ('*' + $nov + '*')) { $novemberTotal++  }
+        if ($fileName -like ('*' + $dec + '*')) { $decemberTotal++  }
+        if ($fileName -like ('*' + $jan + '*')) { $januaryTotal++   }
+        if ($fileName -like ('*' + $feb + '*')) { $februaryTotal++  }
+        if ($fileName -like ('*' + $mar + '*')) { $marchTotal++     }
+        if ($fileName -like ('*' + $apr + '*')) { $aprilTotal++     }
+        if ($fileName -like ('*' + $may + '*')) { $mayTotal++       }
+        if ($fileName -like ('*' + $jun + '*')) { $juneTotal++      }
+        if ($fileName -like ('*' + $jul + '*')) { $julyTotal++      }
+        if ($fileName -like ('*' + $aug + '*')) { $augustTotal++    }
+        if ($fileName -like ('*' + $sep + '*')) { $septemberTotal++ }
+        if ($fileName -like ('*' + $oct + '*')) { $octoberTotal++   }
     }
 }
 
@@ -59,8 +80,15 @@ Write-Host '----------------------------------------'
 Write-Host ('Grand total PDF files: ' + $allPdfTotal)
 Write-Host ''
 Write-Host 'Totals by month:'
-Write-Host ($nov + '  : ' + $novemberTotal)
-Write-Host ($dec + '   : ' + $decemberTotal)
-Write-Host ($jan + '   : ' + $januaryTotal)
-Write-Host ($feb + '  : ' + $februaryTotal)
-Write-Host ($mar + '     : ' + $marchTotal)
+Write-Host ($nov + ' : ' + $novemberTotal)
+Write-Host ($dec + '  : ' + $decemberTotal)
+Write-Host ($jan + '  : ' + $januaryTotal)
+Write-Host ($feb + ' : ' + $februaryTotal)
+Write-Host ($mar + '    : ' + $marchTotal)
+Write-Host ($apr + '  : ' + $aprilTotal)
+Write-Host ($may + '     : ' + $mayTotal)
+Write-Host ($jun + '    : ' + $juneTotal)
+Write-Host ($jul + '    : ' + $julyTotal)
+Write-Host ($aug + ' : ' + $augustTotal)
+Write-Host ($sep + ' : ' + $septemberTotal)
+Write-Host ($oct + ' : ' + $octoberTotal)
